b70931f408409f3d7bb92ce0a0c9d9c67b531fd3
Nazarena Aranda <nazarenaaranda15@gmail.com>
2025-04-28 01:00:17 +0000

    basic async

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -6,7 +6,7 @@ import asyncio
 import random
 
 
-async def wait_random(max_delay:int=10):
+async def wait_random(max_delay: int = 10):
     """takes in an integer argument"""
     rand = random.uniform(0, max_delay)
     await asyncio.sleep(rand)
