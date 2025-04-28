eb6240243e5f319a557983afc13887df244c1948
Nazarena Aranda <nazarenaaranda15@gmail.com>
2025-04-28 00:54:56 +0000

    basic async

---
+++ python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+asynchronous coroutine
+"""
+import asyncio
+import random
+
+
+async def wait_random(max_delay=10):
+    """takes in an integer argument"""
+    rand = random.uniform(0, max_delay)
+    await asyncio.sleep(rand)
+    return rand
