f46750c21103ac72e1fb83b26e642602fd1e0131
Nazarena Aranda <nazarenaaranda15@gmail.com>
2025-04-28 00:25:44 +0000

    function make mulptiplier

---
+++ python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+type-annotated function make_multiplier
+"""
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """that takes a float multiplier as argument and returns a function"""
+    return lambda x: x*x
