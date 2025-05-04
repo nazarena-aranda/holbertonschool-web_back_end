0b48151290b07013a0200a33abad2a1e3bb69870
Nazarena Aranda <nazarenaaranda06@gmail.com>
2025-05-04 21:50:34 +0000

    simple pagination

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -7,5 +7,5 @@ Write a function named index_range that takes two integer arguments
 def index_range(page, page_size) -> tuple:
     """The function should return a tuple of size"""
     start = (page - 1) * page_size
-    end = page * page_size
-    return (start, end)
+    end = start * page_size
+    return start, end
---
+++ pagination/1-simple_pagination.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""
+Copy index_range from the previous task and the following class into your code
+"""
+
+import csv
+from typing import List, Tuple
+
+
+def index_range(page, page_size) -> tuple:
+    """The function should return a tuple of size"""
+    start = (page - 1) * page_size
+    end = start * page_size
+    return start, end
+
+
+class Server:
+      """Server class to paginate a database of popular baby names.
+      """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset"""
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]  # Skip header
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Return a page of the dataset"""
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+
+        start, end = index_range(page, page_size)
+        dataset = self.dataset()
+
+        return dataset[start:end]
