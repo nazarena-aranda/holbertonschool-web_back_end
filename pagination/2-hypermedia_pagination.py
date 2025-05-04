847db1a7c6ae59b0927bd25a930277d8cb05b37e
Nazarena Aranda <nazarenaaranda06@gmail.com>
2025-05-04 22:07:43 +0000

    hypermedia

---
+++ pagination/2-hypermedia_pagination.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+"""
+Copy index_range from the previous task and the following class into your code
+"""
+
+import csv
+from typing import List, Tuple
+import math
+
+
+def index_range(page, page_size) -> tuple:
+    """The function should return a tuple of size"""
+    start = (page - 1) * page_size
+    end = page * page_size
+    return start, end
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset"""
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Return a page of the dataset"""
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+
+        start, end = index_range(page, page_size)
+        dataset = self.dataset()
+
+        return dataset[start:end]
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
+        """Implement a get_hyper method that takes the same arguments"""
+        data = self.get_page(page, page_size)
+        total_items = len(self.dataset())
+        total_pages = math.ceil(total_items / page_size)
+
+        return {
+            "page_size": len(data),
+            "page": page,
+            "data": data,
+            "next_page": page + 1 if page < total_pages else None,
+            "prev_page": page - 1 if page > 1 else None,
+            "total_pages": total_pages
+        }
