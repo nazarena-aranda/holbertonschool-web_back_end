cbe7bfe5b0438b348b8bf697ffb3db01d2fbeece
Nazarena Aranda <nazarenaaranda06@gmail.com>
2025-05-04 23:57:28 -0300

    commands of Mongo

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -5,15 +5,15 @@ from pymongo import MongoClient
 if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
     nginx_collection = client.logs.nginx
-    total = len(list(mongo_collection.find()))
+    total = mongo_collection.count_documents({})
     print("{} logs".format(total))
 
-    get_amount = mongo_collection.find({"method": "GET"})
-    post_amount = mongo_collection.find({"method": "POST"})
-    put_amount = mongo_collection.find({"method": "PUT"})
-    patch_amount = mongo_collection.find({"method": "PATCH"})
-    delete_amount = mongo_collection.find({"method": "DELETE"})
-    status_check_amount = mongo_collection.find({"method": "GET"})
+    get_amount = mongo_collection.count_documents({"method": "GET"})
+    post_amount = mongo_collection.count_documents({"method": "POST"})
+    put_amount = mongo_collection.count_documents({"method": "PUT"})
+    patch_amount = mongo_collection.count_documents({"method": "PATCH"})
+    delete_amount = mongo_collection.count_documents({"method": "DELETE"})
+    status_check_amount = mongo_collection.count_documents({"method": "GET", "path": "/status"})
 
     print("Methods:")
     print("\t method GET: {}".format(get_amount))
