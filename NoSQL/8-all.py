b4b75140f6a7030ed2d9978306f0a9116b48aad2
Nazarena Aranda <nazarenaaranda06@gmail.com>
2025-05-04 23:13:12 -0300

    commands of Mongo

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -7,5 +7,5 @@ from pymongo import MongoClient
 
 
 def list_all(mongo_collection):
+    """Python function that lists all documents in a collection"""
     return list(mongo_collection.find())
-    
