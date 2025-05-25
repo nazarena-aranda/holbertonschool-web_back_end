b88d7097d9110cf5e5e6448486cada4d0350f14b
Nazarena Aranda <nazarenaaranda06@gmail.com>
2025-05-25 16:20:22 +0000

    task pendients

---
+++ Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,26 @@
+const fs = require('fs').promises;
+
+function countStudents(path) {
+  return fs.readFile(path, 'utf8')
+    .then((data) => {
+      const lines = data.split('\n').filter((line) => line.trim() !== '');
+      const students = lines.slice(1);
+
+      const fields = {};
+      for (const student of students) {
+        const [firstname, , , field] = student.split(',');
+        if (!fields[field]) fields[field] = [];
+        fields[field].push(firstname);
+      }
+
+      console.log(`Number of students: ${students.length}`);
+      for (const [field, names] of Object.entries(fields)) {
+        console.log(`Number of students in ${field}: ${names.length}. List: ${names.join(', ')}`);
+      }
+    })
+    .catch(() => {
+      throw new Error('Cannot load the database');
+    });
+}
+
+module.exports = countStudents;
